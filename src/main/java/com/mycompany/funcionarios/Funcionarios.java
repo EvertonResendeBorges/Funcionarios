42b60683bfc9dfabf3fcfa40c31bbc1d87556185
unknown <evertonrborges12@gmail.com>
2025-01-10 20:36:03 -0300

    verifica se a lista está vazia e se há o funcionário cadastrado

--- src/main/java/com/mycompany/funcionarios/Funcionarios.java
+++ src/main/java/com/mycompany/funcionarios/Funcionarios.java
@@ -21,6 +21,7 @@ public class Funcionarios {
         List<String> funcionario = new ArrayList<>();
         
         int opcao;
+        String nomeFuncionario;
       
         
         try{
@@ -28,6 +29,7 @@ public class Funcionarios {
                 System.out.println("1- Mostrar funcionários");
                 System.out.println("2- Adicionar novo funcionário");
                 System.out.println("3- Remover funcionário");
+                System.out.println("4- Número de funcionários");
                 System.out.println("---Para sair digite qualquer outro número---");
                 
                 opcao = s.nextInt();
@@ -35,9 +37,14 @@ public class Funcionarios {
                 
                 switch(opcao) {
                     case 1: 
-                        for(String funcionarios : funcionario) {
+                        if(funcionario.isEmpty()) {
+                            System.out.println("Não há nenhum funcionário cadastrado");
+                        } else {
+                            for(String funcionarios : funcionario) {
                             System.out.println (funcionarios);
+                            }
                         }
+                       
                         break;
                         
                     case 2: 
@@ -47,14 +54,25 @@ public class Funcionarios {
                         
                     case 3: 
                         System.out.println("Qual funcionário deseja remover?");
-                        funcionario.remove(s.nextLine());
+                        nomeFuncionario = s.nextLine();
+                        
+                        if (funcionario.contains(nomeFuncionario)) {
+                            funcionario.remove(nomeFuncionario);
+                        } else {      
+                            System.out.println("Não existe funcionário com esse nome");
+                        }
+                        
                         break;
                         
+                    case 4: 
+                        System.out.println("Número de funcionários: "+funcionario.size());
+                    break;
+                    
                     default: 
                         System.out.println("Saindo...");
                 }
                 
-            } while (opcao > 0 && opcao < 4);
+            } while (opcao > 0 && opcao < 5);
         } catch (Exception e) {
             System.out.println("Digite apenas valores inteiros");
         }
